f35d9dcd22570b6ce0ec9766544048998c45ebd6
NguyenCong2011 <cong201120001@gmail.com>
2025-04-27 22:05:00 +0700

    fix showquesstion

---
+++ src/main/java/com/example/english/demo/dto/request/WebSocketMessage.java
@@ -0,0 +1,15 @@
+package com.example.english.demo.dto.websocket;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class WebSocketMessage {
+    private String type;
+    private String sender;
+    private String receiver;
+    private Object data;
+} 
