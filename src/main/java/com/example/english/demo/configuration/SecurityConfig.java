2c546b9b01227dcfb7fcf5fdb1cd3349365314c5
NguyenCong2011 <cong201120001@gmail.com>
2025-05-11 15:41:50 +0700

    add script for refeshToken

--- src/main/java/com/example/english/demo/configuration/SecurityConfig.java
+++ src/main/java/com/example/english/demo/configuration/SecurityConfig.java
@@ -4,7 +4,6 @@ package com.example.english.demo.configuration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -14,7 +13,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @EnableMethodSecurity
 @Configuration
